f8cc37d3647e6ad6b8051d415fff83cd3fcd95a8
thanapol-six <thanapol@six.network>
2020-07-07 11:25:06 +0700

    add notification

--- src/Hyperledger.Aries.Routing.Mediator/Handlers/MediatorForwardHandler.cs
+++ src/Hyperledger.Aries.Routing.Mediator/Handlers/MediatorForwardHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hyperledger.Aries.Agents;
 using Hyperledger.Aries.Contracts;
@@ -6,9 +7,14 @@ using Hyperledger.Aries.Extensions;
 using Hyperledger.Aries.Features.Routing;
 using Hyperledger.Aries.Routing;
 using Hyperledger.Aries.Storage;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Hyperledger.Aries.Routing
 {
+    
     /// <summary>
     /// Mediator Forward Handler
     /// </summary>
@@ -18,6 +24,9 @@ namespace Hyperledger.Aries.Routing
         private readonly IWalletService walletService;
         private readonly IRoutingStore routingStore;
         private readonly IEventAggregator eventAggregator;
+        private string inboxNotificationEndpoint;
+        private string apiKey;
+        private Boolean notifyEdgeClient;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MediatorForwardHandler"/> class.
@@ -36,6 +45,13 @@ namespace Hyperledger.Aries.Routing
             this.walletService = walletService;
             this.routingStore = routingStore;
             this.eventAggregator = eventAggregator;
+            
+            if (inboxNotificationEndpoint != null && Environment.GetEnvironmentVariable("Inbox_Notification_Endpoint").Length >0)
+            {
+                notifyEdgeClient = true;
+                inboxNotificationEndpoint = Environment.GetEnvironmentVariable("Inbox_Notification_Endpoint");
+                apiKey = Environment.GetEnvironmentVariable("Inbox_Notification_ApiKey");
+            }
         }
 
         /// <inheritdoc />
@@ -49,6 +65,8 @@ namespace Hyperledger.Aries.Routing
             var inboxItemRecord = new InboxItemRecord { ItemData = message.Message.ToJson(), Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds() };
             await recordService.AddAsync(edgeWallet, inboxItemRecord);
 
+            await NotifyEdge(inboxId);
+
             eventAggregator.Publish(new InboxItemEvent
             {
                 InboxId = inboxId,
@@ -56,6 +74,43 @@ namespace Hyperledger.Aries.Routing
             });
 
             return null;
+        }
+
+        private async Task<Boolean> NotifyEdge(string inboxId)
+        {
+            if (notifyEdgeClient)
+            {
+
+                var httpClientHandler = new HttpClientHandler();
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                var client = new System.Net.Http.HttpClient(httpClientHandler);
+
+                HttpContent content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("request_id", inboxId),
+                    new KeyValuePair<string, string>("topic", "notification"),
+                    new KeyValuePair<string, string>("body", "Message from mediator")
+                });
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                content.Headers.Add("x-api-key", apiKey);
+
+                var response = await client.PostAsync(inboxNotificationEndpoint, content);
+              
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string respContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Return from notify {respContent}");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Failed to notify.");
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
